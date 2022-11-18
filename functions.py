07fb291c167a154d749ffe918e1cbfa72787328f
Hugo Siva Rodrigues <84785758+HugoSilvaRodrigues@users.noreply.github.com>
2022-11-18 14:39:39 -0300

    Update functions.py
    Add the function load_and_prep_image

--- functions.py
+++ functions.py
@@ -49,3 +49,21 @@ def plot_loss_curves(history):
   plt.title('Accuracy')
   plt.xlabel('Epochs')
   plt.legend()
+
+def load_and_prep_image(filename, img_shape=224, scale=True):
+  """
+  Get a image and resize to (224, 224, 3).
+  
+(bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
